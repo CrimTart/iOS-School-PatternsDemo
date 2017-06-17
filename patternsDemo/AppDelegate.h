f48aef9b7ae510c90ff050ef32ef937cc6918144
user <iOS-School-1@iMac-user-4.local>
2017-06-17 11:44:38 +0300

    Initial Commit

---
+++ patternsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  patternsDemo
+//
+//  Created by iOS-School-1 on 17.06.17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ patternsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  patternsDemo
+//
+//  Created by iOS-School-1 on 17.06.17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
